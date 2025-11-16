67807a90c51bc039ca1bb53ed5eefaaf3d54e385
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-11-16 18:10:02 +0800

    catalogue module

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "component" {
+    
+}
+
+variable "rule_priority" {
+
+}
+variable "domain_name" {
+    default = "deepthi.cloud"
+}
